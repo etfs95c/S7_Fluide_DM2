ca66427c3914f672caab68ea81ac6bd39a807a53
etfs95c <37842868+etfs95c@users.noreply.github.com>
2026-01-03 18:59:56 +0100

    Added rounding

--- main.py
+++ main.py
@@ -19,26 +19,26 @@ def beta_with_M_Theta(M_1, Theta):
     else:
         Beta = np.degrees(np.arctan(roots))
     
-    print("The value of \u03B2 for a strong shock is :\n\t\u03B2 = ", Beta[0] ,"\nThe value of \u03B2 for a weak shock is :\n\t\u03B2 = ", Beta[1])
+    print("The value of \u03B2 for a strong shock is :\n\t\u03B2 = ", round(Beta[0],3) ,"\nThe value of \u03B2 for a weak shock is :\n\t\u03B2 = ", round(Beta[1],3))
     print(roots)
     return Beta
 
-def beta_after_deviation(M_1, P_1, Theta, w):
-    Beta = beta_with_M_Theta(M_1, Theta)[w]
+def beta_after_deviation(M_1, P_1, Theta, w): #w=1 for weak shock, w=0 for strong shock
+    Beta = round(beta_with_M_Theta(M_1, Theta)[w],3)
 
     gamma = 1.4
 
     Mn_1 = M_1*np.sin(np.radians(Beta))
     Mn_2 = np.sqrt((1 + (gamma-1)/2) * Mn_1**2)/(gamma*Mn_1**2 - (gamma-1)/2)
-    M_2 = (Mn_2/(np.sin(np.radians(Beta-Theta))))
+    M_2 = round((Mn_2/(np.sin(np.radians(Beta-Theta)))),3)
 
-    P_2 = P_1*(1 + (2*gamma)/(gamma+1)*(Mn_1**2 - 1))
+    P_2 = round(P_1*(1 + (2*gamma)/(gamma+1)*(Mn_1**2 - 1)),3)
     print("\nFor \u03B8 = ", Theta,"°, and M\u2081 =", M_1,"and P\u2081 =", M_1,"atm, we get\n \u03B2 = ", Beta,"°\tM\u2082 = ", M_2,"\tP\u2082 = ", P_2,"atm")
     return M_2, P_2
 
 
 print("Hello World")
 
-beta_after_deviation(100, 1, 20, 1)
-
+beta_after_deviation(3, 1, 20, 1)
+#beta_after_deviation()
 print("Hello World")
