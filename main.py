5c110ccd242cb0804b46c632d79ce0e1384232db
etfs95c <37842868+etfs95c@users.noreply.github.com>
2026-01-03 16:25:33 +0100

    Replacing math function with numpy function to prevent calculation error and strating to implement Q3 answer

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import math
 import sys
 
 def beta_with_M_Theta(M_1, Theta):
-    Theta = math.radians(Theta)
+    Theta_rad = np.radians(Theta)
     gamma = 1.4
-    coef = [(gamma-1+(2/M_1**2))*math.tan(Theta), (-2+(2/M_1**2)), (gamma+1+(2/M_1**2))*math.tan(Theta)]
+    coef = [(gamma-1+(2/M_1**2))*np.tan(Theta_rad), (-2+(2/M_1**2)), (gamma+1+(2/M_1**2))*np.tan(Theta_rad)]
     print("\nHello1 \n")
 
     roots = np.roots(coef)
@@ -21,6 +21,15 @@ def beta_with_M_Theta(M_1, Theta):
     
     print("The value of \u03B2 for a strong shock is :\n\t\u03B2 = ", Beta[0] ,"\nThe value of \u03B2 for a weak shock is :\n\t\u03B2 = ", Beta[1])
     print(roots)
+
+def beta_after_deviation(M_1, P_1, theta):
+    Beta_s = beta_with_M_Theta[0]
+    Beta_w = beta_with_M_Theta[1]
+
+    gamma = 1.4
+
+    Mn_1_s = M_1*np.sin(np.radians(Beta_s))
+    Mn_2_s = np.sqrt((1 + (gamma-1)/2) * Mn_1_s**2)/(gamma*Mn_1_s**2 - (gamma-1)/2)
 
 print("Hello World")
 
